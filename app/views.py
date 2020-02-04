a47b59aba17e98a85fa1fff962687f5f427409c9
beli302 <belindashirkiz2@gmail.com>
2020-02-04 10:29:08 +0300

    added macro.html file in templates folder

--- app/views.py
+++ app/views.py
@@ -1,15 +1,33 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/news/<int:news_id>')
 def news(news_id):
-def index():    
+
+	'''
+	Function to view news page
+	'''
+	news = get_news(id)
+	title = f'{id}'
+
+	return render_template('news.html',title= title,news = news_articles)
+
+
+@app.route('/')
+def index():
 
     '''
-    View news page function that returns the news details page and its data
+    function that returns the index page and its data
     '''
-    return render_template('news.html',id = news_id)
 
-    title = 'Home - Welcome to The News update review'
-    return render_template('index.html', title = title)
+    # Getting popular movie
+    general_news = get_sources('general')
+    technology_news = get_sources('technology')
+    business_news = get_sources('business')
+    sports_news = get_sources('sports')
+    entertainment_news = get_sources('entertainment')
+    
+    
+    return render_template('index.html', title = title, general = general_news, technology = technology_news, business = business_news, sports = sports_news, entertainment = entertainment_news )
